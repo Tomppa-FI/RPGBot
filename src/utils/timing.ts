4982b84dde530b61415511a8401ab24440ae5df9
Tomppa-FI <TomppaFI@protonmail.com>
2020-06-20 15:08:06 +0300

    Formatting timer output.

--- src/utils/timing.ts
+++ src/utils/timing.ts
@@ -3,5 +3,7 @@ export const QuestStartDelayMS = (speedModifier: number) => 120000 * speedModifi
 export const msToString = (ms: number) => {
   const minutes = Math.floor((ms / 1000) % 3600 / 60);
   const seconds = Math.floor((ms / 1000) % 3600 % 60);
-  return `${minutes}:${seconds}`;
+  return `${minutes ? `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}` : ""} ${seconds ? `${seconds} ${seconds === 1 ? 'second' : 'seconds'}` : ""}`;
 }
+
+export const promisifyDelay = (delayInMs: number) => new Promise(resolve => setTimeout(resolve, delayInMs));
