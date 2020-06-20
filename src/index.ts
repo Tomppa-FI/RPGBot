e82a6c23681a6ad4987874af722ecf5ba70d7549
Tomppa-FI <TomppaFI@protonmail.com>
2020-06-20 11:32:11 +0300

    Added Discord Connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { Client } from "discord.js";
+
+const client = new Client();
+client.once('ready', () => {
+  console.log(`Bot Connected to Discord as ${client.user.tag}`);
+})
+
+try {
+  client.login(process.env.DISCORD_TOKEN);
+} catch (e) {
+  console.log(e);
+}
