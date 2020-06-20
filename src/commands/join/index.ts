b7bc199161e0fe7e3020c3d131defcff95f76f2d
Tomppa-FI <TomppaFI@protonmail.com>
2020-06-20 16:46:41 +0300

    Quick bugfix - isPlayerInQuest reversed.

--- src/commands/join/index.ts
+++ src/commands/join/index.ts
@@ -5,6 +5,6 @@ export default function handleJoin(msg: Message) {
   if (!(msg.channel instanceof TextChannel)) return msg.reply(`You can only join a Quest from a TextChannel.`);
   if (!isQuestActive(msg.guild.id)) return msg.reply('No Quest is currently running for this Guild.');
   if (!isQuestJoinable(msg.guild.id)) return msg.reply(`This Quest is currently not accepting players.`);
-  if (!isPlayerInQuest(msg)) return msg.reply(`You are already part of this Quest!`);
+  if (isPlayerInQuest(msg)) return msg.reply(`You are already part of this Quest!`);
   addPlayerToQuest(msg);
 }
